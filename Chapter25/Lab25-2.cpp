fb0712b532ef7402d60316f4bc5d053770b558d0
ThiriZueZun <684244101@mail.pbru.ac.th>
2025-08-05 11:36:21 +0700

    Update Lab25-2.cpp

--- Chapter25/Lab25-2.cpp
+++ Chapter25/Lab25-2.cpp
@@ -17,6 +17,9 @@ class Base_class {
 
 class Triangle : public Base_class{
 	public:
+	Triangle(){
+		base =10; height = 2;
+	}
 		void findArea(){
 			std::cout <<"Area of Triangle is " << (0.5 * base * height) << std::endl;
 		}
@@ -27,9 +30,42 @@ class Triangle : public Base_class{
 };
 
 class Rectangle : public Base_class{
-	
+	public:
+		Rectangle(){
+		width = 5; length = 10;
+	}
+		void findArea(){
+			std::cout << "Area of rectangle is " << (width * length) << std::endl;
+		}
+		void perimeter(){
+			std::cout << "Perimeter of rectangle id " << (2 * (width+length)) << std::endl;
+		}
 };
 
 class Circle : public Base_class{
+	public:	
+	Circle(double rr){
+		radius = rr;
+	}
 	
+	void findArea(){
+		std::cout<<"Area of circle is " << (M_PI * radius * radius) << std::endl;
+	}
+	void perimeter(){
+		std::cout << "Perimeter of circle is " << (2.0 * M_PI * radius) << std::endl;
+	}
 };
+
+int main(){
+	Triangle tr;
+	tr.findArea();
+	tr.perimeter();
+	
+	Rectangle rec;
+	rec.findArea();
+	rec.perimeter();
+	
+	Circle cr(10);
+	cr.findArea();
+	cr.perimeter();
+}
